04785a9d204fd96b4e42e2864c117a7d9348d843
alex <pisonalex@gmail.com>
2025-12-09 15:58:12 +0000

    Feat: simple game - 'ILoveYou' with right hand continues game

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
+import time
+
 from graph import Node
+from myGestureRecognizer.gestureRecogniserRefactor import GestureRecognizerApp
 
 if __name__ == "__main__":
     start = Node("Start of the game. Make decision and move.")
     leftDecision: Node = Node("ups, you died")
     rightDecision: Node = Node("you win. yey.")
 
-    start.addNode(leftDecision)
-    start.addNode(rightDecision)
+    start.addNode(("ILoveYou", "Left"), leftDecision)
+    start.addNode(("ILoveYou", "Right"), rightDecision)
+
+    rightDecision.addNode(("ILoveYou", "Right"), start)
 
     curNode: Node = start
+    recogniser: GestureRecognizerApp = GestureRecognizerApp()
     while True:
         print(curNode.text)
-        decision = int(input(f"Pick a decision: {[node.text for node in curNode.adjacencyList]}: "))
+        print(f"Pick a decision: {[node.text for node in curNode.adjacencyList.values()]}: ")
+        decision = recogniser.run()
         curNode = curNode.getNode(decision)
+        time.sleep(5)
