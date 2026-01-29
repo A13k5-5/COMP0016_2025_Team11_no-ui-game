aa8f052fefc3e27fb4e47680bf273360ef3f003c
alex <pisonalex@gmail.com>
2026-01-29 18:38:36 +0000

    Refactor: cleanup before using better text2speech model
    Issue #9

--- graph.py
+++ graph.py
@@ -1,18 +1,9 @@
-from tts import generateAudioFile
-import pygame
-
-pygame.init()
-
 class Node:
     def __init__(self, text: str):
         self._text = text
-        self.audioFilePath = f"./audioFiles/{text[:10]}.wav"
-        # generateAudioFile(text, self.audioFilePath)
         self.adjacencyList: dict[tuple[str, str], str] = {}
 
     def getText(self):
-        # run the audiofile found at self.audioPath
-        pygame.mixer.Sound(self.audioFilePath).play()
         return self._text
 
     def addNode(self, gesture: tuple[str, str], newNode):
