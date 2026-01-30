3440f973836ca9b1ce1fb0788bc49f0b842493cf
alex <pisonalex@gmail.com>
2026-01-30 18:50:20 +0100

    Fix: minor changes as proposed by Damla
    Issue #9

--- graph.py
+++ graph.py
@@ -1,7 +1,7 @@
 class Node:
     def __init__(self, text: str):
         self._text = text
-        self.adjacencyList: dict[tuple[str, str], str] = {}
+        self.adjacencyList: dict[tuple[str, str], Node] = {}
 
     def getText(self):
         return self._text
@@ -10,7 +10,7 @@ class Node:
         self.adjacencyList[gesture] = newNode
 
     def getNode(self, gesture: tuple[str, str]):
-        return self.adjacencyList[gesture]
+        return self.adjacencyList.get(gesture)
 
     def __str__(self):
         return self._text
