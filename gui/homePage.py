48b96e5c289337f645b090262ac8e7294d9a69e8
Damla Bortucen <damla.bortucen.24@ucl.ac.uk>
2026-02-06 14:10:19 +0000

    feat: connect game creation page to the corresponding button on the home page. Issue #6

--- gui/homePage.py
+++ gui/homePage.py
@@ -1,21 +1,32 @@
 import sys
+from gameCreationPage import GameCreationPage
 from PySide6 import QtWidgets, QtCore, QtGui
 
 class HomePage(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
-        # set title, size and layout type (vertical box)
-        self.setWindowTitle("No-UI-Game Editor")
+        self._setup_window_layout("No-UI-Game")
+        self._create_widgets()
+        self._add_widgets()
+           
+    def _setup_window_layout(self, window_title : str):
+        """
+        Set the window title, size and layout.
+        """
+        self.setWindowTitle(window_title)
         self.resize(800, 600)
         self.layout = QtWidgets.QVBoxLayout(self)
-        
-        # create widgets
-        self.new_game_button = QtWidgets.QPushButton("Create a New Game")
-        self.edit_game_button = QtWidgets.QPushButton("Edit Game")
-        self.text = QtWidgets.QLabel("Hello World", alignment=QtCore.Qt.AlignCenter)
 
-        # add widgets
+    def _create_widgets(self):
+        self.new_game_button = QtWidgets.QPushButton("Create a New Game")
+        self.new_game_button.clicked.connect(self.open_game_creator)
+        self._creation_window = None
+
+        self.edit_game_button = QtWidgets.QPushButton("Edit Game")
+        self.text = QtWidgets.QLabel("No-UI Game", alignment=QtCore.Qt.AlignCenter)
+
+    def _add_widgets(self):
         self.layout.addWidget(self.text)
 
         # horizontal box layout for the 2 buttons
@@ -25,6 +36,9 @@ class HomePage(QtWidgets.QWidget):
 
         self.layout.addLayout(self.button_row)
 
+    def open_game_creator(self):
+        self._creation_window = GameCreationPage()
+        self._creation_window.show()
 
 def run():
     app = QtWidgets.QApplication([])
