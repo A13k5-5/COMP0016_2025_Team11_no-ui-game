e25a65c7e21f1d5be1238393d6c40f3347a2bbe2
alex <pisonalex@gmail.com>
2026-02-16 18:32:28 +0000

    Feat: GUI working with the GameSaver
    Issue #26 #28

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ import os
 
 if __name__ == "__main__":
     player = GamePlayer()
-    player.playGame(os.path.join(os.path.dirname(__file__), "Cool Game"))
+    player.playGame(os.path.join(os.path.dirname(__file__), "saved_games", "Lord of the rings"))
+
+# if __name__ == "__main__":
+#     from gui.gameCreationPage import run
+#     run()
