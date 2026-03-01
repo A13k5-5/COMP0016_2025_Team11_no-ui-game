07d5cbf03e26bf973ac90e8744b9449add6f5550
alex <pisonalex@gmail.com>
2026-03-01 17:58:25 +0000

    feat: gamePlayer can be run from cli with the path as argument

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-# if __name__ == "__main__":
-#     from gamePlayer import GamePlayer
-#     import os
-#     player = GamePlayer()
-#     player.playGame(os.path.join(os.path.dirname(__file__), "saved_games", "Lord of the rings"))
+import sys
+import os
 
 if __name__ == "__main__":
-    from gui.homePage import run
-    run()
+
+    import gamePlayer
+
+    player = gamePlayer.GamePlayer()
+    # player.playGame(os.path.join(os.path.dirname(__file__), "saved_games", "simple game"))
+    # player.playGame("./saved_games/simple_game")
+    player.playGame(sys.argv[1])
+
+# if __name__ == "__main__":
+#     from gui.homePage import run
+#     run()
