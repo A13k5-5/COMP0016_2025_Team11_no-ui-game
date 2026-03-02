b6fbc7dc300fb0ae3c3cfddaaa49af4f8bc18734
alex <pisonalex@gmail.com>
2026-03-02 22:35:01 +0000

    Feat: game stored as a .zip file that is loaded and unzipped

--- gamePlayer/gamePlayer.py
+++ gamePlayer/gamePlayer.py
@@ -4,9 +4,9 @@ import time
 
 from graph import Node
 import myGestureRecognizer
-import storageManager
 
 from gesture import EnumGesture
+import storageManager.game_load
 
 
 class GamePlayer:
@@ -14,7 +14,8 @@ class GamePlayer:
     Class to play the interactive story game.
     """
     def __init__(self):
-        self.game_loader: storageManager.GameLoader = storageManager.GameLoader()
+        self.game_loader: storageManager.game_load.GameLoader = storageManager.game_load.GameLoader()
+
         self.recogniser: myGestureRecognizer.VideoGestureRecogniser = myGestureRecognizer.VideoGestureRecogniser()
 
     def _playAudio(self, game_path: str, audio_filename: str):
@@ -29,14 +30,14 @@ class GamePlayer:
 
     def playGame(self, game_path: str):
         try:
-            root_node: Node = self.game_loader.load_graph(game_path)
+            root_node, game_folder = self.game_loader.load_graph(game_path)
         except Exception as e:
             print(f"Failed to load graph from file: {e}")
             return
 
-        self._startGameLoop(root_node, game_path)
+        self._startGameLoop(root_node, game_folder)
 
-    def _startGameLoop(self, startNode: Node, game_path: str):
+    def _startGameLoop(self, startNode: Node, game_folder: str):
         """
         Throws TimeoutError if no gesture is detected within TIMEOUT_TIME seconds.
         """
@@ -48,7 +49,7 @@ class GamePlayer:
             self._listOptions(curNode)
             
             # Play current scene audio
-            self._playAudio(game_path, curNode.audio_filename)
+            self._playAudio(game_folder, curNode.audio_filename)
 
             # Ask recognizer for a decision (expects a tuple like ("ILoveYou", "Left"))
             decision: EnumGesture = self.recogniser.get_gesture(list(curNode.adjacencyList.keys()))
--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-from gui.playerPage import run
+# Compilation mode, support OS-specific options
+# nuitka-project-if: {OS} in ("Windows", "Linux", "Darwin", "FreeBSD"):
+#    nuitka-project: --mode=onefile
+# nuitka-project-else:
+#    nuitka-project: --mode=standalone
+
+# The PySide6 plugin covers qt-plugins
+# nuitka-project: --enable-plugin=pyside6
+# nuitka-project: --include-qt-plugins=qml
+
+import sys
+
+import gamePlayer
+import playerPage
 
 if __name__ == "__main__":
-    run()
+
+    if len(sys.argv) > 1:
+        player = gamePlayer.GamePlayer()
+        player.playGame(sys.argv[1])
+    else:
+        playerPage.run()
