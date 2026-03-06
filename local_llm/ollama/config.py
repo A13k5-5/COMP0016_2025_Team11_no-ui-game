94ad1aab37ed99787f4154be181a7cec471fc82c
alex <pisonalex@gmail.com>
2026-03-06 14:11:15 +0100

    Feat: using openvino for structured output
    Issue #37

--- local_llm/ollama/config.py
+++
@@ -1,2 +0,0 @@
-model_id: str = "OpenVINO/Phi-3-mini-4k-instruct-int4-ov"
-model_path: str = "Phi-3-mini-4k-instruct-int4-ov"
--- local_llm/ollama/download_llm.py
+++
@@ -1,5 +0,0 @@
-import huggingface_hub as hf_hub
-
-from .config import model_id, model_path
-
-hf_hub.snapshot_download(model_id, local_dir=model_path)
