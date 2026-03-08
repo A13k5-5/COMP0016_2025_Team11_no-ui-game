60c57777fe300d5b4915ebe4955923cb6b8dbcb6
alex <pisonalex@gmail.com>
2026-03-08 12:29:40 +0100

    Refactor: GameGenerator class to encapsulate the game generation
    Issue #37

--- local_llm/structured_output.py
+++ local_llm/structured_output.py
@@ -3,11 +3,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import argparse
-import json
-from openvino_genai import GenerationConfig, LLMPipeline, StructuredOutputConfig, ChatHistory
+from openvino_genai import LLMPipeline
 
-from graph.serial_graph import SerialGraph
-from prompts import SYS_MESSAGE
+from local_llm.GameGenerator import GameGenerator
 
 
 
@@ -16,39 +14,23 @@ def main():
     parser.add_argument("model_dir", help="Path to the model directory. It should contain the OpenVINO model files.")
     args = parser.parse_args()
 
-    device = "CPU"  # GPU can be used as well
-    pipe = LLMPipeline(args.model_dir, device)
+    pipe: LLMPipeline = LLMPipeline(args.model_dir, "CPU")
+    game_generator: GameGenerator = GameGenerator(pipe)
 
-    config = GenerationConfig()
 
     print(
         "This is a smart assistant that generates an adventure game graph."
     )
 
     while True:
-        try:
-            prompt = input("> ")
-        except EOFError:
-            break
+        prompt = input("> ")
 
         if prompt in ["exit", "quit", "bye"]:
             break
 
         # configuring the system message and the structured output config for the pipeline
-        history = ChatHistory()
-        history.append({"role": "system", "content": SYS_MESSAGE})
-
-        config.structured_output_config = StructuredOutputConfig(
-            json_schema=json.dumps(SerialGraph.model_json_schema())
-        )
-        config.do_sample = True
-        config.temperature = 0.8
-
-        history.append({"role": "user", "content": prompt})
-
-        decoded_results = pipe.generate(history, config)
-        json_response = decoded_results.texts[0]
-        print(json_response)
+        story: str = game_generator.generate_game(prompt)
+        print(f"\nGenerated story graph: \n{story}")
 
 
 if "__main__" == __name__:
