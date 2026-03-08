1301623631d3a2694e56cd66b32d5ee9de5e6a55
alex <pisonalex@gmail.com>
2026-03-08 11:56:37 +0100

    refactor: sys_message moved to prompts.py
    Issue #37

---
+++ local_llm/prompts.py
@@ -0,0 +1,15 @@
+SYS_MESSAGE: str = (
+    "You generate JSON objects based on the user's request. You will generate a list of JSON objects that follow the SerialNode schema. "
+    "A SerialNode object represents a node in a graph of a text-based game. The user will give you their ideas"
+    " about how many nodes should be in the game, and what should be their high-level content. "
+    "It is your job to generate the full story. Make sure to follow the JSON schema for the SerialNode. "
+    "To help you understand the SerialNode class better, i will explain what each field means: "
+    "- id: a unique integer identifier for the node. It should start from 0 and increment by 1 for each new node. "
+    "- text: the text that will be shown to the player when they reach this node."
+    "- left_option: the text that will be shown to the player for the left option. This field can be empty if there is no left option. "
+    "- right_option: the text that will be shown to the player for the right option."
+    "adjacency_list: a dictionary that maps the player's choice left(1) or right(0) to the id of the next node. "
+    "it is absolutely necessary to make sure that the id of the next node exists in the generated JSON objects. "
+    "- is_win: a boolean that indicates whether this node is a winning node or not."
+    "Please use double quotes for JSON keys and values. "
+)
