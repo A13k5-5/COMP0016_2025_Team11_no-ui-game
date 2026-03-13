b5d18ff5090e9fcda5655e5174ef18ce8078c681
alex <pisonalex@gmail.com>
2026-03-13 19:43:53 +0000

    feat: local_main
    made it into loop with structured output
    
    Issue #52

--- game_generation_local_llm/llm_main.py
+++ game_generation_local_llm/llm_main.py
@@ -9,9 +9,6 @@ class Country(BaseModel):
     population: int
 
 if __name__ == "__main__":
-    model_path: str = os.path.join(os.path.dirname(__file__), "models", "TinyLlama_1_1b_v1_ov")
-    prompt: str = "What is the capital of England? And what is its population?"
-
     config = GenerationConfig()
     config.temperature = 0.1
     config.max_new_tokens = 40
@@ -19,10 +16,20 @@ if __name__ == "__main__":
 
     history = ChatHistory()
     history.append({"role": "system", "content": "You are a helpful assistant."})
-    history.append({"role": "user", "content": prompt})
 
     print("Loading model...")
+    model_path: str = os.path.join(os.path.dirname(__file__), "models", "TinyLlama_1_1b_v1_ov")
     pipe: LLMPipeline = LLMPipeline(model_path, "GPU")
-    print("Model loaded. Generating response...")
-    decoded_results = pipe.generate(history, config)
-    print(decoded_results.texts[0])
+    while True:
+
+        prompt: str = input(">> ")
+        if prompt == "bye":
+            break
+
+        history.append({"role": "user", "content": prompt})
+
+        print("Generating response...")
+        decoded_results = pipe.generate(history, config)
+        print(decoded_results.texts[0])
+
+        history.pop()
