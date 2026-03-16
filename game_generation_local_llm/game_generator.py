deedb9adce5ecad3c42725b6cf75526c0ff9e6a1
alex <pisonalex@gmail.com>
2026-03-16 19:31:29 +0000

    config: minor .gitignore config changes
    Issue #56

--- game_generation_local_llm/game_generator.py
+++ game_generation_local_llm/game_generator.py
@@ -10,8 +10,8 @@ from game_generation_local_llm.story_generator.story_generator import StoryGener
 
 class GameGenerator:
     def __init__(self):
-        model_path: str = os.path.join(os.path.dirname(__file__), "model_path")
-        self.pipe: LLMPipeline = LLMPipeline(model_path, "CPU")
+        model_path: str = os.path.join(os.path.dirname(__file__), "models", "TinyLlama-1.1B-Chat-v1.0_ov")
+        self.pipe: LLMPipeline = LLMPipeline(model_path, "GPU")
         self.blueprint_generator: BlueprintGenerator = BlueprintGenerator(self.pipe)
         self.game_generator: StoryGenerator = StoryGenerator(self.pipe)
 
