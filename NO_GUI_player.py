b3f4731f96837ccf713b7dba5db4490e57b0d222
alex <pisonalex@gmail.com>
2026-03-19 11:41:25 +0000

    config: organizing nuitka for no_GUI_player
    Issue #52

--- NO_GUI_player.py
+++ NO_GUI_player.py
@@ -1,9 +1,11 @@
 # nuitka-project: --mode=standalone
-
-# nuitka-project: --enable-plugin=pyside6
-# nuitka-project: --include-qt-plugins=qml
 # nuitka-project: --windows-disable-console
 
+# for pyside6
+# nuitka-project: --enable-plugin=pyside6
+# nuitka-project: --include-qt-plugins=qml
+
+# recognizer file
 # nuitka-project: --include-data-files={MAIN_DIRECTORY}/src/myGestureRecognizer/gesture_recognizer.task=src/myGestureRecognizer/gesture_recognizer.task
 if __name__ == "__main__":
     import sys
